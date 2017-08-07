563cc174cdba2365697cc3a410fc49a0432eff9e
HyunJomi <hyunjomi@HyunJomiui-MacBook-Air.local>
2017-08-07 17:47:39 +0900

    임현정 - project file add

---
+++ WeatherSound/WeatherSound/MainTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  MainTableViewCell.swift
+//  WeatherSound
+//
+//  Created by HyunJomi on 2017. 8. 2..
+//  Copyright © 2017년 HyunJung. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    static let reuseId = "musicCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.prepareCell()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func prepareCell(){
+        
+        self.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: .extraLight)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        
+        self.backgroundView = blurEffectView
+
+        
+        
+       
+        self.titleLabel.textAlignment = .left
+        self.titleLabel.frame = CGRect(x: self.albumImageView.frame.maxX+10, y: self.frame.minY+5, width: self.frame.width-80, height: 50)
+        
+        self.albumImageView.backgroundColor = .gray
+        self.albumImageView.frame = CGRect(x: 20, y: self.frame.minY+5, width: 50, height: 50)
+        
+        
+    }
+    
+    func set(title: String, artist: String){
+        
+        let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: "\(title)", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 15, weight: UIFontWeightThin), NSForegroundColorAttributeName: UIColor(red:0.29, green:0.26, blue:0.28, alpha:1.00)])
+        attributedString.append(NSAttributedString(string: "\n\(artist)", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 13, weight: UIFontWeightThin), NSForegroundColorAttributeName: UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.00)]))
+        
+        self.titleLabel.attributedText = attributedString
+    }
+    
+}
---
+++ WeatherSound/WeatherSound/MyPageViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MyPageViewController.swift
+//  WeatherSound
+//
+//  Created by HyunJomi on 2017. 8. 7..
+//  Copyright © 2017년 HyunJung. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var myPageTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.myPageTableView.delegate = self
+        self.myPageTableView.dataSource = self
+        
+        self.prepareView()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func prepareView(){
+        
+        let rect = self.view.bounds
+        
+        self.myPageTableView.frame = CGRect(x: 0, y: 0, width: rect.width, height: rect.height)
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
+        
+        return cell
+    }
+}
